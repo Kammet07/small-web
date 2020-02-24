bb4d1edd2e44e4b94e6d63252aab63de403e84bd
kammet07 <pavel.pupentsov@educanet.cz>
2020-02-24 13:56:25 +0100

    done1

--- registration.php
+++ registration.php
@@ -1,27 +1,23 @@
 <?php
 session_start();
 $_SESSION['login'] = false;
+$newUsername = $_POST['username'] ?? null;
+$newPassword = $_POST['password'] ?? null;
+
+if (is_string($newUsername) && is_string($newPassword)) {
+    $_SESSION['username'] = $newUsername;
+    $_SESSION['password'] = $newPassword;
+}
+
+$username = $_SESSION['username'] ?? null;
+$password = $_SESSION['password'] ?? null;
+
+if ($username && $password) {
+    echo "Welcome";
+    $_SESSION['login'] = true;
+}
 if ($_SESSION['login']) {
     header('location: lorem.php');
-} else {
-    $newUsername = $_POST['username'] ?? null;
-    $newPassword = $_POST['password'] ?? null;
-
-    if (is_string($newUsername) && is_string($newPassword)) {
-        $_SESSION['username'] = $newUsername;
-        $_SESSION['password'] = $newPassword;
-    }
-
-    $username = $_SESSION['username'] ?? null;
-    $password = $_SESSION['password'] ?? null;
-
-    if ($username && $password) {
-        echo "Welcome";
-        $_SESSION['login'] = true;
-    }
-    if ($_SESSION['login']) {
-        header('location: lorem.php');
-    }
 }
 
 ?>
